d851c30897687a4c13dc5479793d97b056262472
Wildan Novaldi <wildanv10@gmail.com>
2021-09-11 15:19:59 +0700

    Move config variable to env

--- src/services/firebase/initializeApp.js
+++ src/services/firebase/initializeApp.js
@@ -10,8 +10,7 @@ const firebaseConfig = {
   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  databaseURL:
-    "https://boilerplate-a2e96-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
